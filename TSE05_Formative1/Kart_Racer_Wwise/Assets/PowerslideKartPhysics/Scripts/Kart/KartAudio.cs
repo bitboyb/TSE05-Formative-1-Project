b8e63dc187ef2ca992e3c13110ab5dee4145df8d
bitboyb <62619775+bitboyb@users.noreply.github.com>
2023-06-18 00:45:23 +0100

    initial project

---
+++ TSE05_Formative1/Kart_Racer_Wwise/Assets/PowerslideKartPhysics/Scripts/Kart/KartAudio.cs
@@ -0,0 +1,168 @@
+﻿// Copyright (c) 2020 Justin Couch / JustInvoke
+using UnityEngine;
+using System.Collections;
+
+namespace PowerslideKartPhysics
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Kart))]
+    //Class for playing kart sounds
+    public class KartAudio : MonoBehaviour
+    {
+        Kart theKart;
+        public bool zeroDoppler = true;
+     
+
+        private AK.Wwise.RTPC speedRTPC;
+        private AK.Wwise.Switch groundSurface;
+
+        private float kartSpeed;
+        private bool isBoosting;
+        private bool isDrifting;
+
+        //Wwise events
+        public AK.Wwise.Event engineSnd;
+        public AK.Wwise.Event jumpSnd;
+        public AK.Wwise.Event landSnd;
+        public AK.Wwise.Event collisionSnd;
+        public AK.Wwise.Event boostStartSnd;
+        public AK.Wwise.Event boostStopSnd;
+        public AK.Wwise.Event boostFailSnd;
+        public AK.Wwise.Event tireSnd;
+        public AK.Wwise.Event stopTireSnd;
+        public AK.Wwise.Event itemUseSnd;
+        public AK.Wwise.Event itemHitSnd;
+
+        private void Awake()
+        {
+            theKart = GetComponent<Kart>();
+
+            //Post Wwise event
+            engineSnd.Post(gameObject);
+
+            isDrifting = false;
+
+        }
+
+        private void Update()
+        {
+            if (theKart == null) { return; }
+
+            //Get Kart velocity variable
+            kartSpeed = theKart.localVel.z;
+
+            AkSoundEngine.SetRTPCValue("Speed", kartSpeed);
+            //Debug.Log(theKart.drifting);
+
+            if (theKart.boostReserve < 0.1 && isBoosting == true)
+            {
+                PlayBoostStopSnd();
+            }
+
+            //check if drifting and run skidding audio on correct surface
+            DriftingCheck();
+
+        }
+
+        //Play the jump sound
+        public void PlayJumpSnd()
+        {
+            jumpSnd.Post(gameObject);
+        }
+
+        //Play the land sound
+        public void PlayLandSnd()
+        {
+            landSnd.Post(gameObject);
+        }
+
+        //Wrapper for the collision sound playing function
+        //The position vector is a dummy parameter for the sake of working with the collision UnityEvent in the Kart class
+        public void PlayCollisionSnd(Vector3 pos, Vector3 vel)
+        {
+            PlayCollisionSnd(vel.magnitude);
+        }
+
+        //Play a random collision sound with the given volume factor
+        public void PlayCollisionSnd(float volume)
+        {
+            collisionSnd.Post(gameObject);
+        }
+
+        //Play the boost start sound
+        public void PlayBoostStartSnd()
+        {
+
+            if (isBoosting == false)
+            {
+                boostStartSnd.Post(gameObject);
+
+                isBoosting = true;
+            }
+            
+        }
+
+        public void PlayBoostStopSnd()
+        {
+            boostStopSnd.Post(gameObject);
+
+            isBoosting = false;
+        }
+
+        //Play the boost fail sound
+        public void PlayBoostFailSnd()
+        {
+            boostFailSnd.Post(gameObject);
+        }
+
+        //Play the sound for using and item
+        public void PlayItemUseSnd()
+        {
+            itemUseSnd.Post(gameObject);
+        }
+
+        //Play the sound for getting hit by an item
+        public void PlayItemHitSnd()
+        {
+            itemHitSnd.Post(gameObject);
+        }
+
+        public void DriftingCheck()
+        {
+            GroundSurfacePreset currentsurface = theKart.GetWheelSurface();
+
+            //only run if drifting
+            if (theKart.drifting == true)
+            {
+                //get current switch state and store in switchState variable
+                uint switchState;
+
+                AkSoundEngine.GetSwitch("GroundSurface", gameObject, out switchState);
+
+                //only change switch if different from current switch state
+                if (currentsurface == true && switchState != 1481171086)
+                {
+                    AkSoundEngine.SetSwitch("GroundSurface", "OffRoad", gameObject);
+                }
+                else if (currentsurface == false && switchState != 3769881715)
+                {
+                    AkSoundEngine.SetSwitch("GroundSurface", "Tarmac", gameObject);
+                }
+            }
+
+            if (theKart.drifting == true && theKart.grounded == true && isDrifting == false)
+            {
+                tireSnd.Post(gameObject);
+
+                isDrifting = true;
+            }
+
+            if (theKart.grounded == false ^ theKart.drifting == false && isDrifting == true)
+            {
+                stopTireSnd.Post(gameObject);
+
+                isDrifting = false;
+            }
+        }
+    }
+}
---
+++ TSE05_Formative1/Kart_Racer_Wwise/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,70 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BOOST_FAIL = 2216498074U;
+        static const AkUniqueID PLAY_BOOST_START = 1048210378U;
+        static const AkUniqueID PLAY_BOOST_STOP = 1418047778U;
+        static const AkUniqueID PLAY_COLLISION = 1553219250U;
+        static const AkUniqueID PLAY_ENGINE_3WHEELER = 3119007570U;
+        static const AkUniqueID PLAY_ENGINE_BIKE = 1987163492U;
+        static const AkUniqueID PLAY_ENGINE_KART = 1910152473U;
+        static const AkUniqueID PLAY_ITEM_HIT = 3201268741U;
+        static const AkUniqueID PLAY_ITEM_USE = 3300110083U;
+        static const AkUniqueID PLAY_JUMP = 3689126666U;
+        static const AkUniqueID PLAY_LAND = 4285282925U;
+        static const AkUniqueID PLAY_TIRESKID = 1953146513U;
+        static const AkUniqueID STOP_TIRESKID = 3995482915U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace GROUNDSURFACE
+        {
+            static const AkUniqueID GROUP = 953937849U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID OFFROAD = 1481171086U;
+                static const AkUniqueID TARMAC = 3769881715U;
+            } // namespace SWITCH
+        } // namespace GROUNDSURFACE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SPEED = 640949982U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MASTER_SOUNDBANK = 2469504869U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
